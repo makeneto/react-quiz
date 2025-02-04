38f8754a6a0a02170ca5b94d24f78fdddd1774a8
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-02-04 16:16:37 +0100

    Update on Finish Screen

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,7 +1,7 @@
 import { useQuiz } from "../contexts/QuizContext";
 
 function FinishScreen() {
-  const { points, maxPossiblePoints, highscore, dispatch } = useQuiz();
+  const { points, maxPossiblePoints, highScore, dispatch } = useQuiz();
 
   const percentage = (points / maxPossiblePoints) * 100;
 
@@ -18,7 +18,7 @@ function FinishScreen() {
         <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
         {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
-      <p className="highscore">(Highscore: {highscore} points)</p>
+      <p className="highscore">(High Score: {highScore} points)</p>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "restart" })}
